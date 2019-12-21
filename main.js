4b53df0f464a4541da5e9480144607ee370024fe
Brandon Diamond <1036976+brandondiamond@users.noreply.github.com>
2019-12-20 21:01:31 -0500

    Facts and tweaks.

--- main.js
+++ main.js
@@ -5,25 +5,25 @@ function animateCity(W, H) {
   const LT = "#fff";
   const P = 10;
   const S = 2000;
-  
+
   function snap(v, g) {
     return g ? (v ? Math.round(v / g) * g : 0) : v;
   }
-  
+
   function rnd(m, M, c) {
     let r = Math.random();
     if (c) r = c(r);
     return (m + r * (M - m));
   }
-  
+
   function rs(m, M, g, c) {
     return snap(rnd(m, M, c), g);
   }
-  
+
   function lst(a) {
     return a[a.length-1];
   }
-  
+
   function computeSkyline(w, h, wB, hB, pS, s, d, g) {
     let points = [[0, h]];
     let lights = [];
@@ -53,7 +53,7 @@ function animateCity(W, H) {
       points.push([lst(points)[0], h]);
     return { s: points, l: lights };
   }
-  
+
   function centerSkyline(gfx, w, h, g) {
     const sw = lst(gfx.s)[0] - gfx.s[0][0];
     const sm = gfx.s.map((p) => [p[0] + snap(w / 2 - sw / 2, g), p[1]]);
@@ -62,12 +62,12 @@ function animateCity(W, H) {
     sm.push([w, h]);
     return { s: sm, l: lm };
   }
-  
+
   function drawSkyline(draw, gfx) {
     // TODO: animate stroke
     const gradient = draw.gradient('linear', function(add) {
       add.stop(0,   FG, 1);
-      add.stop(0.6, FG, 0.8);
+      add.stop(0.8, FG, 0.8);
       add.stop(1,   FG, 0.4);
     }).rotate(90);
     return draw.polyline(gfx.s).fill('none')
@@ -76,27 +76,27 @@ function animateCity(W, H) {
                .animate(1000)
                .opacity(0.4);
   }
-  
+
   function drawLights(draw, gfx, p, s, ss, d) {
     let skip = 0;
     const dl = () =>
     gfx.l.forEach((l) => {
       const dl = Math.max(1, ss + rnd(-d,d));
       const dr = rnd(200, 1000);
-      const wt = 5 * ss + rnd(0,d);
+      const wt = 2 * ss + rnd(0,d);
       if (skip-- <= 0 && rnd(0, 100) < p) {
         const r = draw.rect(s, s).move(l[0]-s/2, l[1]-s/2).fill(LT).opacity(0);
         const b = Math.max(0.3, Math.min(1, (H - l[1]) / H));
         r.animate(dr, dl).opacity(b).delay(wt).animate(dr).opacity(0).after(() => r.remove());
       }
     });
-    dl(); setInterval(dl, 8*ss);
+    dl(); setInterval(dl, 5*ss);
   }
-  
+
   let draw = SVG().size(W, H).addTo("#city");
   //let gfx = centerSkyline(computeSkyline(W / 2, H, 50, H, 20, 30, 20, G), W, H, G);
   let gfx = computeSkyline(W, H, 50, H, 20, 30, 20, G);
-  
+
   drawSkyline(draw, gfx);
   drawLights(draw, gfx, 10, 2, S, 1500);
 }
